4cb009af54bd009a65ef66eba23fad853d84555a
Lucyane Iskandar-Fröde <iskandar.lucyane@gmail.com>
2023-07-11 20:35:03 +0200

    add script

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+let index = 0;
 
+function show_image(i) {
+  index += i;
+
+  let images = document.getElementsByClassName("image");
+
+  let dots = document.getElementsByClassName("dot")
+
+  // create for loop for blocking the images if we click prev or next button 
+
+    for (i = 0; i < images.length; i++)
+    images[i].style.display= "none";
+
+  // create for loop for clearing the class name if we click prev or next button 
+  // replace is for deleting
+
+    for (i = 0; i < dots.length; i++)
+    dots[i].className = dots[i].className.replace(" active", "")
+
+  if(index > images.length - 1)
+  index = 0;
+
+  if(index < 0)
+   index = images.length - 1;
+   
+   // for displaying images
+
+   images[index].style.display = "block";
+   dots[index].className += " active"
+
+}
